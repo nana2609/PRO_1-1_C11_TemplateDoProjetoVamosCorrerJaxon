dcab0f2fce0bcaa54aee037a9ca655b251a6e319
nana2609 <102555110+nana2609@users.noreply.github.com>
2022-04-11 18:27:41 -0300

    jaxon correndo

--- sketch.js
+++ sketch.js
@@ -9,16 +9,17 @@ boyImg = loadAnimation("Runner-1.png","Runner-2.png");
 }
 
 function setup(){
- createCanvas(400,400);
+createCanvas(400,400);
 
 // Movendo o fundo 
-
-
-
-
+path = createSprite (200,200);
+path.addImage(pathImg);
+path.velocityY = 8;
 //Criando menino que corre 
 
-
+boy = createSprite(180,340,30,30);
+boy.addAnimation ("correndo",boyImg);
+boy.scale = 0.06;
 
 // Criando Boundary (Limite) esquerdo  
 leftBoundary=createSprite(0,0,100,800);
@@ -26,28 +27,33 @@ leftBoundary.visible = false;
 
 
 //Crie Boundary direito 
+rightBoundary=createSprite(410,0,100,800);
+rightBoundary.visible = false;
 
 }
 
 
 function draw() {
 background(0);
-path.velocityY = 4;
+path.velocityY = 8;
 
 // Menino se movendo no eixo X com o mouse
 edges= createEdgeSprites();
 boy.collide(edges[3]);
-
-
+boy.x= World.mouseX;
+boy.collide(leftBoundary);
+boy.collide(rightBoundary);
 
  //Reiniciar o fundo
- 
+if (path.y > 400){
+path.y = path.width/2;
+}
 
 
 
- drawSprites();
+drawSprites();
 
- }
+}
 
 
 
